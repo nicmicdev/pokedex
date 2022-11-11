6991b2c01f84e68ebe9a74dc45c527542ebbf698
Niclas Michel <niclas.michel@MacBook-Pro.local>
2022-11-11 15:04:37 +0800

    detailview and search function working

--- script.js
+++ script.js
@@ -3,14 +3,52 @@ let currentPokemon;
 let allPokemons = [];
 let pokemonContainer = getElement('pokemon-container');
 let cardContainer = getElement('card-container');
-let lengthOfTypes = currentPokemon['types'].length; 
+let pokemonNames = [];
+
+
+// let searchbar = getElement('searchbar');
+
+// searchbar.addEventListener('keyup', (e) => {
+//     let searchString = e.target.value;
+//     let filteredPokemons = allPokemons.filter( pokemon => {
+//         return pokemon.name.includes(searchString);
+//     });
+//     console.log(filteredPokemons);
+
+// })
+
+
+function searchPokemons() {
+    let searchString = getElement('searchbar').value;
+    searchString = searchString.toLowerCase();
+    pokemonContainer.innerHTML = "";
+    for (let i = 0; i < 152; i++) {
+        let name = allPokemons[i]['name'];
+        if (name.toLowerCase().includes(searchString)){
+            console.log(name)
+            renderFilteredCardsSmall(i);
+        }
+    }
+}
+
+function renderFilteredCardsSmall(i)  {
+    let pokemon = allPokemons[i];
+    pokemonContainer.innerHTML += generateFilteredCardSmallHTML(i,pokemon);
+    let lengthOfTypes = currentPokemon['types'].length; 
+    if (lengthOfTypes == 2){
+        getElement(`card-s-${i}`).innerHTML += generateSecondTypeSmallHTML();
+    }
+    paintFilteredSmallCards(i);
+}
+
+
+
 
 
 //FETCHING POKEMON DATA FROM API
 async function loadAllPokemons() {
     pokemonContainer.innerHTML = '';
     for (let i = 1; i < 152; i++) {
-        const pokemon = allPokemons[i];
         let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let response = await fetch(url);
         currentPokemon = await response.json();
@@ -21,32 +59,63 @@ async function loadAllPokemons() {
 }
 
 
+
+// function filterPokemons() {
+//     let inputvalue = getElement('searchbar').value;
+//     inputvalue = inputvalue.toLowerCase();
+//     console.log (inputvalue);
+
+//     pokemonContainer.innerHTML = "";
+
+//     for (let i = 0; i < 152; i++) {
+//         const pokemon = allPokemons[i]['name'];
+//         if (pokemon.includes(inputvalue)) {
+//             pokemonContainer.innerHTML += generatePokemonCardSmallHTML(i);
+//         }
+//     }
+// }
+
+
+
+
+
 function renderSmallPokemonCards(i)  {
     pokemonContainer.innerHTML += generatePokemonCardSmallHTML(i);
+    let lengthOfTypes = currentPokemon['types'].length; 
     if (lengthOfTypes == 2){
         getElement(`card-s-${i}`).innerHTML += generateSecondTypeSmallHTML();
     }
     paintSmallCards(i);
 }
 
-function renderDetailCards(i) {
 
 
+
+function openCard(i){
+    getElement('card-container').classList.remove('d-none');
+    getElement('overlay').classList.remove('d-none');
+    getElement('body').classList.add('oflow-y-hid');
+    getElement('body').classList.remove('oflow-y-unset');
+    renderDetailCard(i);
 }
 
 
-function openCard(){
-    document.getElementById('card-container').classList.remove('d-none');
-    document.getElementById('overlay').classList.remove('d-none');
-    document.getElementById('body').classList.add('oflow-y-hid');
-    document.getElementById('body').classList.remove('oflow-y-unset');
+ function renderDetailCard(i) {
+    let pokemon = allPokemons[i-1];
+    cardContainer.innerHTML = generateDetailCardHTML(i, pokemon);
+    let lengthOfTypes = pokemon['types'].length; 
+    if (lengthOfTypes == 2){
+        getElement(`card-${i}`).innerHTML += generateSecondTypeHTML(pokemon);
+    }
+    paintDetailCard(i, pokemon);
 }
+
 
 function closeCard(){
-    document.getElementById('card-container').classList.add('d-none');
-    document.getElementById('overlay').classList.add('d-none');
-    document.getElementById('body').classList.add ('oflow-y-unset');
-    document.getElementById('body').classList.remove ('oflow-y-hidden');
+    getElement('card-container').classList.add('d-none');
+    getElement('overlay').classList.add('d-none');
+    getElement('body').classList.add ('oflow-y-unset');
+    getElement('body').classList.remove ('oflow-y-hidden');
 }
 
 
@@ -68,9 +137,10 @@ function openSearch() {
 
 
 function closeSearch() {
+    getElement('searchbar').value = '';
     getElement('search-icon').classList.remove('d-none');
     getElement('close-icon').classList.add('d-none');
-    getElement('searchbar').value = '';
+    searchPokemons();
 }
 
 // function openCard(i) {
@@ -140,6 +210,105 @@ function paintSmallCards(i){
     }
 }
 
+function paintFilteredSmallCards(i){
+    let type = allPokemons[i]['types'][0]['type']['name'];
+    if (type == 'grass') {
+        getElement(`card-s-${i}`).classList.add('bg-grass');
+    }
+    if (type == 'fire') {
+        getElement(`card-s-${i}`).classList.add('bg-fire');
+    }
+    if (type == 'water') {
+        getElement(`card-s-${i}`).classList.add('bg-water');
+    }
+    if (type == 'bug') {
+        getElement(`card-s-${i}`).classList.add('bg-bug');
+    }
+    if (type == 'normal') {
+        getElement(`card-s-${i}`).classList.add('bg-normal');
+    }
+    if (type == 'poison') {
+        getElement(`card-s-${i}`).classList.add('bg-poison');
+    }
+    if (type == 'electric') {
+        getElement(`card-s-${i}`).classList.add('bg-electric');
+    }
+    if (type == 'ground') {
+        getElement(`card-s-${i}`).classList.add('bg-ground');
+    }
+    if (type == 'fairy') {
+        getElement(`card-s-${i}`).classList.add('bg-fairy');
+    }
+    if (type == 'fighting') {
+        getElement(`card-s-${i}`).classList.add('bg-fighting');
+    }
+    if (type == 'psychic') {
+        getElement(`card-s-${i}`).classList.add('bg-psychic');
+    }
+    if (type == 'rock') {
+        getElement(`card-s-${i}`).classList.add('bg-rock');
+    }
+    if (type == 'ghost') {
+        getElement(`card-s-${i}`).classList.add('bg-ghost');
+    }
+    if (type == 'ice') {
+        getElement(`card-s-${i}`).classList.add('bg-ice');
+    }
+    if (type == 'dragon') {
+        getElement(`card-s-${i}`).classList.add('bg-dragon');
+    }
+}
+
+
+function paintDetailCard(i,pokemon){
+    let type = pokemon['types'][0]['type']['name'];
+    if (type == 'grass') {
+        getElement(`card-${i}`).classList.add('bg-grass');
+    }
+    if (type == 'fire') {
+        getElement(`card-${i}`).classList.add('bg-fire');
+    }
+    if (type == 'water') {
+        getElement(`card-${i}`).classList.add('bg-water');
+    }
+    if (type == 'bug') {
+        getElement(`card-${i}`).classList.add('bg-bug');
+    }
+    if (type == 'normal') {
+        getElement(`card-${i}`).classList.add('bg-normal');
+    }
+    if (type == 'poison') {
+        getElement(`card-${i}`).classList.add('bg-poison');
+    }
+    if (type == 'electric') {
+        getElement(`card-${i}`).classList.add('bg-electric');
+    }
+    if (type == 'ground') {
+        getElement(`card-${i}`).classList.add('bg-ground');
+    }
+    if (type == 'fairy') {
+        getElement(`card-${i}`).classList.add('bg-fairy');
+    }
+    if (type == 'fighting') {
+        getElement(`card-${i}`).classList.add('bg-fighting');
+    }
+    if (type == 'psychic') {
+        getElement(`card-${i}`).classList.add('bg-psychic');
+    }
+    if (type == 'rock') {
+        getElement(`card-${i}`).classList.add('bg-rock');
+    }
+    if (type == 'ghost') {
+        getElement(`card-${i}`).classList.add('bg-ghost');
+    }
+    if (type == 'ice') {
+        getElement(`card-${i}`).classList.add('bg-ice');
+    }
+    if (type == 'dragon') {
+        getElement(`card-${i}`).classList.add('bg-dragon');
+    }
+}
+
 // EVENT LISTENER
 
 document.addEventListener('keydown', function(event){      
@@ -160,3 +329,19 @@ document.addEventListener('keydown', function(event){
 //     document.getElementById('pokemon-name').innerHTML = currentPokemon ['name'];
 //     document.getElementById('pokemon-img').src = currentPokemon['sprites']['front_shiny'];
 // }
+
+
+
+// function paintProgressBar(pokemon) {
+//     for (let i = 0; i < 6; i++) {
+//         let statValue = (pokemon['stats'][i]['base_stat'])*0.649351;
+//         if (statValue < 51.33) {
+//             getElement(`progress${i}`).classList.add('bg-danger');
+//         } else if (statValue > 51.33 && statValue < 102.66) {
+//             getElement(`progress${i}`).classList.add('bg-warning');
+//         } else {
+//             getElement(`progress${i}`).classList.add('bg-success');
+//         }
+//     }
+// }
+
--- template.js
+++ template.js
@@ -1,5 +1,5 @@
 function generatePokemonCardSmallHTML(i) {
- return `
+    return `
         <div class="pokemon-card-s" id="card-s-${i}" onclick="openCard(${i})">
             <h3 class="card-s-heading">${capitalize(currentPokemon['name'])}</h3>
             <h5 class="card-s-id">#${i}</h5>
@@ -9,11 +9,92 @@ function generatePokemonCardSmallHTML(i) {
 `
 }
 
-function generateSecondTypeSmallHTML(){
-   return ` <span class="card-s-type-2">${currentPokemon['types'][1]['type']['name']}</span> `
+function generateFilteredCardSmallHTML(i,pokemon) {
+    return `
+        <div class="pokemon-card-s" id="card-s-${i}" onclick="openCard(${i+1})">
+            <h3 class="card-s-heading">${capitalize(pokemon['name'])}</h3>
+            <h5 class="card-s-id">#${i+1}</h5>
+            <span class="card-s-type">${pokemon['types'][0]['type']['name']}</span>
+            <img class="card-s-img" src= ${pokemon['sprites']['other']['official-artwork']['front_default']}>
+        </div>
+`
+}
+
+function generateDetailCardHTML(i,pokemon) {
+    return `
+    <div class="card-top" id="card-${i}">
+        <h2 class="card-heading">${capitalize(pokemon['name'])}</h2>
+        <h2 class="card-id">#${i}</h2>
+        <h2 class="card-close" onclick="closeCard()" title="Close (ESC)">x</h2>
+        <h2 class="card-arrow-left" title="Previous Pokémon"><</h2>
+        <h2 class="card-arrow-right" title="Next Pokémon">></h2>
+        <span class="card-type">${pokemon['types'][0]['type']['name']}</span>
+        <img class="card-pic" src="${pokemon['sprites']['other']['official-artwork']['front_default']}">
+        <span class="card-experience" title="The base experience gained for defeating this Pokémon.">Base Experience: <b>${pokemon['base_experience']}</b></span>
+        <span class="card-weight" title="Hectogram">Weight: <b>${pokemon['weight']}</b></span>
+        <span class="card-h8" title="Decimetres">Height: <b>${pokemon['height']}</b></span>
+    </div>
+
+    <div class="card-bottom">
+        <h5 class="bottom-heading">Base Stats</h5>
+
+        <div class="stats-container">
+        
+            <div class="stats-titles">
+                <span>HP</span>
+                <span>Attack</span>
+                <span>Defense</span>
+                <span>Special-Attack</span>
+                <span>Special-Defense</span>
+                <span>Speed</span>
+            </div>
+
+            <div class="stats-numbers">
+                <span>${pokemon['stats'][0]['base_stat']}</span>
+                <span>${pokemon['stats'][1]['base_stat']}</span>
+                <span>${pokemon['stats'][2]['base_stat']}</span>
+                <span>${pokemon['stats'][3]['base_stat']}</span>
+                <span>${pokemon['stats'][4]['base_stat']}</span>
+                <span>${pokemon['stats'][5]['base_stat']}</span>
+            </div>
+
+            <div class="stats-progress">
+            
+                <div class="progress" >
+                    <div class="progress-bar bg-secondary" id="progress-0" style="width: ${(pokemon['stats'][0]['base_stat'])*0.649351}%;" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="154"></div>
+                </div>
+
+                <div class="progress" >
+                    <div class="progress-bar  bg-secondary" id="progress-1" style="width: ${(pokemon['stats'][1]['base_stat'])*0.649351}%;" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="154"></div>
+                </div>
+
+                <div class="progress" >
+                    <div class="progress-bar  bg-secondary" id="progress-2" style="width: ${(pokemon['stats'][2]['base_stat'])*0.649351}%;" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="154"></div>
+                </div>
+
+                <div class="progress" >
+                    <div class="progress-bar  bg-secondary" id="progress-3" style="width: ${(pokemon['stats'][3]['base_stat'])*0.649351}%;" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="154"></div>
+                </div>
+
+                <div class="progress" >
+                    <div class="progress-bar bg-secondary" id="progress-4" style="width: ${(pokemon['stats'][4]['base_stat'])*0.649351}%;" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="154"></div>
+                </div>
+
+                <div class="progress" >
+                    <div class="progress-bar bg-secondary" id="progress-5" style="width: ${(pokemon['stats'][5]['base_stat'])*0.649351}%;" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="154"></div>
+                </div>
+            
+            </div>
+
+    </div>
+`
+}
+
+function generateSecondTypeSmallHTML() {
+    return ` <span class="card-s-type-2">${currentPokemon['types'][1]['type']['name']}</span> `
 }
 
 
-function generateSecondTypeHTML(){
-   return ` <span class="card-type-2">${currentPokemon['types'][1]['type']['name']}</span> `
+function generateSecondTypeHTML(pokemon) {
+    return ` <span class="card-type-2">${pokemon['types'][1]['type']['name']}</span> `
 }
